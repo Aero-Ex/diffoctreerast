39c8f81b07cb6fcd7de44945e3f1d89f1ed41c84
Aero-Ex <sumitmathur559@gmail.com>
2025-11-14 18:22:26 +0530

    Add Windows binary wheel build workflow

--- setup.py
+++ setup.py
@@ -1,10 +1,35 @@
 from setuptools import setup
 from torch.utils.cpp_extension import CUDAExtension, BuildExtension
 import os
-os.path.dirname(os.path.abspath(__file__))
+
+root_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Compiler flags for Windows CUDA 11.8 + VS 2022 compatibility
+cxx_flags = []
+nvcc_flags = ["-I" + os.path.join(root_dir, "lib/glm/")]
+
+if os.name == 'nt':  # Windows
+    cxx_flags = [
+        '/D_ALLOW_COMPILER_AND_STL_VERSION_MISMATCH',
+        '/D_SILENCE_ALL_CXX17_DEPRECATION_WARNINGS',
+    ]
+    nvcc_flags.extend([
+        '-allow-unsupported-compiler',
+        '-D_ALLOW_COMPILER_AND_STL_VERSION_MISMATCH',
+        '--expt-relaxed-constexpr',
+        '--expt-extended-lambda',
+    ])
+
+extra_compile_args = {
+    'cxx': cxx_flags,
+    'nvcc': nvcc_flags
+}
 
 setup(
     name="diffoctreerast",
+    version="0.1.0",
+    author="Jianfeng Xiang",
+    description="Differential Octree Rasterization for 3D Generation",
     packages=['diffoctreerast'],
     ext_modules=[
         CUDAExtension(
@@ -33,8 +58,9 @@ setup(
                 # Main
                 "src/ext.cpp"
             ],
-            extra_compile_args={"nvcc": ["-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "lib/glm/")]})
-        ],
+            extra_compile_args=extra_compile_args,
+        )
+    ],
     cmdclass={
         'build_ext': BuildExtension
     }
